f159a958c65c315bcc57be750cd610bf9796ad1b
Polyakov Roman <polyakov.r.v@yandex.ru>
2023-11-28 19:53:57 +0300

    Del terraform

--- terraform/base/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "server_public_ip" {
-  value = yandex_vpc_address.addr.external_ipv4_address[0].address
-}
-
-output "server_local_ip" {
-  value = yandex_compute_instance.vm[0].network_interface[0].ip_address
-}
